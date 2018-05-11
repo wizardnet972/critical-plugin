155ce892f62e3751f26b298f37824fd3dd56e122
shlomi levi <wizardnet972@gmail.com>
2018-05-11 19:33:01 +0300

    1.0.5

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -1,48 +1,49 @@
-const fs = require('fs');
-const {
-    resolve
-} = require('path');
-const webpack = require('webpack');
+import fs from 'fs';
+import path from 'path';
 
-describe("A main suite for running all the cases", function () {
+import webpack from 'webpack';
 
-    const casesDirectory = resolve(__dirname, 'cases');
-    const outputDirectory = resolve(__dirname, 'output');
+
+describe('TestCases', () => {
+
+    const casesDirectory = path.resolve(__dirname, 'cases');
+    const outputDirectory = path.resolve(__dirname, 'output');
 
     for (const directory of fs.readdirSync(casesDirectory)) {
 
         it(`${directory} should compile to the expected result`, (done) => {
-            const directoryForCase = resolve(casesDirectory, directory);
-            const outputDirectoryForCase = resolve(outputDirectory, directory);
+          
+            const directoryForCase = path.resolve(casesDirectory, directory);
+            const outputDirectoryForCase = path.resolve(outputDirectory, directory);
 
-            const webpackConfig = require(resolve(directoryForCase, 'webpack.config.js'));
+            const webpackConfig = require(path.resolve(directoryForCase, 'webpack.config.js'));
 
-            webpack(Object.assign({
+            webpack({
+                mode: 'none',
                 context: directoryForCase,
                 output: {
                     path: outputDirectoryForCase,
                 },
-                mode: 'none'
-            }, webpackConfig), (err, stats) => {
+                ...webpackConfig
+            }, (err, stats) => {
                 if (err) {
                     done(err);
                     return;
                 }
-                done();
-
+              
                 if (stats.hasErrors()) {
                     done(new Error(stats.toString({
-                        context: resolve(__dirname, '..'),
+                        context: path.resolve(__dirname, '..'),
                         errorDetails: true,
                     })));
                     return;
                 }
 
-                const expectedDirectory = resolve(directoryForCase, 'expected');
+                const expectedDirectory = path.resolve(directoryForCase, 'expected');
 
                 for (const file of fs.readdirSync(expectedDirectory)) {
-                    const content = fs.readFileSync(resolve(expectedDirectory, file), 'utf-8');
-                    const actualContent = fs.readFileSync(resolve(outputDirectoryForCase, file), 'utf-8');
+                    const content = fs.readFileSync(path.resolve(expectedDirectory, file), 'utf-8');
+                    const actualContent = fs.readFileSync(path.resolve(outputDirectoryForCase, file), 'utf-8');
                     expect(actualContent).toEqual(content);
                 }
 
--- src/index.js
+++ src/index.js
@@ -5,56 +5,64 @@ const critical = require('critical');
 const path = require('path');
 const fs = require('fs');
 
+const pluginName = 'CriticalPlugin';
+
 class CriticalPlugin {
 
-    constructor(options) {
-        this.options = options || {};
-        this.criticalOptions = this.options.critical || {};
+    constructor(options = { critical: {} }) {
+        this.options = options;
     }
 
     apply(compiler) {
+        compiler.hooks.afterEmit.tapAsync(pluginName, (chunk, callback) => this.onAfterEmit(chunk, callback));
+    }
 
-        compiler.hooks.afterEmit.tapAsync('CriticalPlugin', (chunk, callback) => {
+    onAfterEmit(chunk, callback) {
 
-            const assets = Object.keys(chunk.assets)
-                .filter(key => key && key.toLowerCase().endsWith('.html'))
-                .map(key => chunk.assets[key])
-                .map(asset => ({
-                    asset,
-                    options: Object.assign({
-                        base: path.dirname(asset.existsAt),
-                        src: path.basename(asset.existsAt),
-                        inline: true,
-                    }, this.criticalOptions)
-                }))
-                .map(({
-                        asset,
-                        options
-                    }) =>
-                    critical.generate(options).then(html => {
-                        return this.writeFile(html, asset.existsAt);
-                    }).catch(e => {
-                        console.log(chalk.yellow(`[${options.src} Ignored] ${e.message}. Please report this issue to https://github.com/wizardnet972/critical-plugin/issues.`));
-                    }));
+        let assets = Object.keys(chunk.assets)
+            .filter(key => key && key.toLowerCase().endsWith('.html'))
+            .map(key => chunk.assets[key])
+            .map(asset => this.critical({
+                asset,
+                options: {
+                    base: path.dirname(asset.existsAt),
+                    src: path.basename(asset.existsAt),
+                    inline: true,
+                    ...this.options.critical
+                }
+            }));
+            
+        assets && assets.length && console.info(`${pluginName} `, chalk.yellow('critical your html...'));
+        
+        Promise.all([...assets]).then(() => {
+            console.info(`${pluginName} `, chalk.green('critical complete.'));
+            callback();
+        });
+    }
+ 
+    critical({ asset, options }) {
+        return new Promise((resolve, reject) => {
 
-            if (assets && assets.length) {
-                console.log(chalk.yellow('resolving critical path...'))
-            }
-
-            Promise.all(assets)
-                .then(() => {
-                    console.log(chalk.green('critical path is set.'));
-                    callback();
+            critical.generate(options)
+                .then(html => this.save(asset.existsAt, html).then(() => resolve()))
+                 .catch(e => {
+                     if (e.message.indexOf('No usable stylesheets found in html source') === 0){
+                         console.warn(`${pluginName} `, chalk.yellow(`IGN!  File ignored: ${options.src} at ${e}.`, '\n\r'));
+                         resolve();
+                         return;
+                     }
+                     console.error(`${pluginName} `, chalk.red(`ERR!`), `Please report this issue to`, chalk.yellow(`https://github.com/wizardnet972/critical-plugin/issues`), '\n\r', chalk.red(`File: ${options.src} at ${e}.`, '\n\r'));
+                     reject(e);
                 });
         });
     }
 
-    writeFile(html, filename) {
+    save(filename, html) {
         return new Promise((resolve, reject) => {
 
             if (!filename) return reject(new Error('outputPath & filename must be set to update output file'));
 
-            if (!html) return resolve()
+            if (!html) return resolve();
 
             fs.writeFile(filename, html, (err) => {
                 if (err) {
